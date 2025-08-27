bf73e16db772d80a3802ef9345ec39ffdb272c7d
minhto1306 <lekhaiminh2606@gmail.com>
2025-08-28 01:00:27 +0700

    First commit

---
+++ SecondPrj/src/iuh/fit/se/Rectangle.java
@@ -0,0 +1,13 @@
+package iuh.fit.se;
+
+public class Rectangle {
+
+}
+
+
+/**
+ * @description OOP-Object-Oriented Programming
+ * @author Le Khai Minh
+ * @version 1.0
+ * @created Aug 27, 202511:53:34 PM
+ */
---
+++ SecondPrj/src/iuh/fit/se/TestRectangle.java
@@ -0,0 +1,13 @@
+package iuh.fit.se;
+
+public class TestRectangle {
+
+}
+
+
+/**
+ * @description OOP-Object-Oriented Programming
+ * @author Le Khai Minh
+ * @version 1.0
+ * @created Aug 27, 202511:53:53 PM
+ */
